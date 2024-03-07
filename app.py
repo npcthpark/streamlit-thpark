49270b8c8ccf5eedfbdf90117f7a5d7904744add
npcthpark <70880507+npcthpark@users.noreply.github.com>
2024-03-07 10:39:42 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 view = [100, 150, 30]
-st.write('# Youtube view')
+st.write('# Streamlit view')
+st.title(':flag-kr:')
 view
 st.write('## bar chart') 
 st.bar_chart(view)
-
 import pandas as pd
 sview = pd.Series(view)
-st.write('## Series')
+sview = st.write('## Series')
 sview
